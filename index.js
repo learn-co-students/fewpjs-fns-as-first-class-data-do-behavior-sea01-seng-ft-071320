b14c0b890d8d39cf52660606c155f53f98b71028
bdb2381 <brad.beggs@gmail.com>
2020-08-25 16:36:54 -0700

    Done.

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  timeSplit = time.split(:)
+  timeSplit.parseInt
+  if (time < 1200){
+    return "Good Morning"
+  }
+  else if(time < 1700) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+
+}
 /* Write your implementation of displayMessage() */
